a9ab1d4b037d587d47f681b9702d9d7a4ab697d3
Sawan <sawan.iitkgp@gmail.com>
2020-10-26 10:59:51 +0530

    Remove redundant code

--- fetch_data.sh
+++ fetch_data.sh
@@ -8,10 +8,6 @@ python merge_esnli_train.py ./external/esnli
 python -c "import urllib.request; urllib.request.urlretrieve(\"https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FMNLI.zip?alt=media&token=50329ea1-e339-40e2-809c-10c40afff3ce\", \"./external/MNLI.zip\")"
 unzip -d ./external/ ./external/MNLI.zip
 
-#prepare train/test data for LM finetuning
-python prepare_train_test.py --dataset snli --create_data
-python prepare_train_test.py --dataset mnli --create_data
-
 mkdir cache
 mkdir saved_lm
 mkdir saved_gen
