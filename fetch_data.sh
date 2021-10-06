ef7eb47dd49afff6855358901afca24de27f0eae
Sawan <sawan.iitkgp@gmail.com>
2021-10-06 09:37:30 +0530

    Updated MNLI download path

--- fetch_data.sh
+++ fetch_data.sh
@@ -5,7 +5,7 @@ git clone https://github.com/OanaMariaCamburu/e-SNLI.git ./external/esnli
 python merge_esnli_train.py ./external/esnli
 
 #mnli, from glue
-python -c "import urllib.request; urllib.request.urlretrieve(\"https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FMNLI.zip?alt=media&token=50329ea1-e339-40e2-809c-10c40afff3ce\", \"./external/MNLI.zip\")"
+wget -O ./external/MNLI.zip https://dl.fbaipublicfiles.com/glue/data/MNLI.zip
 unzip -d ./external/ ./external/MNLI.zip
 
 mkdir cache
